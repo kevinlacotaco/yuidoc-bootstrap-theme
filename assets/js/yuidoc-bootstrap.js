73cc7110df76b5b2d0bc4c9401d9bda10ad325fc
tonybaroneee <tonybaroneee@gmail.com>
2012-11-11 16:12:20 -0500

    Re-wrote keystroke shortcuts

--- assets/js/yuidoc-bootstrap.js
+++ assets/js/yuidoc-bootstrap.js
@@ -1,5 +1,16 @@
 $(function() {
 
+    // ************************************************************************* //
+    //  HTML templates
+    // ************************************************************************* //
+
+    // (None)
+
+
+    // ************************************************************************* //
+    //  Functions
+    // ************************************************************************* //
+
     function setUpActiveTab() {
         if(localStorage['main-nav'] !== null){
             $('a[href="'+ localStorage['main-nav'] + '"]').tab('show');
@@ -46,7 +57,72 @@ $(function() {
         });
     }
 
+
+    // ************************************************************************* //
+    //  Initializations + Event listeners
+    // ************************************************************************* //
+
+    $(window).keyup(function(e) {
+        // Listen for 's' key and focus search input if pressed
+        if (e.keyCode === 83) { // 's'
+            $('#api-tabview-filter input').focus();
+        }
+    });
+
+    $('#sidebar').keydown(function(e) {
+        // Cache variables
+        var $this = $(this);
+
+        // Determine if the control/command key was pressed
+        if (e.ctrlKey) {
+            if (e.keyCode === 37) { // left arrow
+                $('#main-nav li a:first').tab('show');
+            } else if (e.keyCode === 39) { // right arrow
+                $('#main-nav li a:last').tab('show');
+            }
+        } else {
+            if (e.keyCode === 40) { // down arrow
+                if ($('#api-tabview-filter input').is(':focus')) {
+                    // Scenario 1: We're focused on the search input; move down to the first li
+                    $this.find('.tab-content .tab-pane.active li:first a').focus();
+                } else if ($this.find('.tab-content .tab-pane.active li:last a').is(':focus')) {
+                    // Scenario 2: We're focused on the last li; move up to search input
+                    $('#api-tabview-filter input').focus();
+                } else {
+                    // Scenario 3: We're in the list but not on the last element, simply move down
+                    nextIndex = $this
+                                    .find('.tab-content .tab-pane.active li')
+                                    .find('a:focus')
+                                    .parent('li').index() + 1;
+                    $this.find('.tab-content .tab-pane.active li:eq('+nextIndex+') a').focus();
+                }
+            } else if (e.keyCode === 38) { // up arrow
+                if ($('#api-tabview-filter input').is(':focus')) {
+                    // Scenario 1: We're focused on the search input; move down to the last li
+                    $this.find('.tab-content .tab-pane.active li:last a').focus();
+                } else if ($this.find('.tab-content .tab-pane.active li:first a').is(':focus')) {
+                    // Scenario 2: We're focused on the first li; move up to search input
+                    $('#api-tabview-filter input').focus();
+                } else {
+                    // Scenario 3: We're in the list but not on the first element, simply move up
+                    nextIndex = $this
+                                    .find('.tab-content .tab-pane.active li')
+                                    .find('a:focus')
+                                    .parent('li').index() - 1;
+                    $this.find('.tab-content .tab-pane.active li:eq('+nextIndex+') a').focus();
+                }
+            }
+            return false;
+        }
+    })
+
+
+    // ************************************************************************* //
+    //  Immediate function calls
+    // ************************************************************************* //
+
     setUpActiveTab();
     bindEventsForPage();
     setUpWidgets();
+
 });
